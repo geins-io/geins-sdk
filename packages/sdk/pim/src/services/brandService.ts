5969ea676bdd47cf137877dd20022995b358fb82
arvidsson-geins <arvidsson@geins.io>
2024-09-15 16:29:27 +0200

    feat: added querys and test bench page

--- packages/sdk/pim/src/services/brandService.ts
+++ packages/sdk/pim/src/services/brandService.ts
@@ -1,36 +1,25 @@
-import type { MenuVariables, MenuType } from '@geins/types';
+// https://docs.geins.io/docs/api/merchant/objects/brand-list-type
+// import type { MenuVariables, MenuType } from '@geins/types';
 import { BaseApiService } from '@geins/core';
 import { queries } from '../graphql';
 //import { parseMenuItem } from '../util/contentParsers';
 export class MenuService extends BaseApiService {
-  private async generateVars(variables: MenuVariables) {
-    if (!variables.menuLocationId) {
-      throw new Error('LocationId is required');
-    }
+  private async generateVars(variables: any) {
     const vars = this.createVariables(variables);
     return vars;
   }
-  async getRaw(variables: MenuVariables) {
+  async getRaw(variables: any) {
     const vars = await this.generateVars(variables);
-    return await this.runQuery(queries.menu, vars);
+    throw new Error('Not implemented');
+    //return await this.runQuery(queries.menu, vars);
   }
-  async get(variables: MenuVariables) {
+  async get(variables: any) {
     const vars = await this.generateVars(variables);
-    return await this.runQueryParsed(queries.menu, vars);
+    throw new Error('Not implemented');
+    //return await this.runQueryParsed(queries.menu, vars);
   }
 
-  protected parseResult(result: any): MenuType {
-    if (!result || !result.data || !result.data.getMenuAtLocation) {
-      throw new Error('Invalid result structure');
-    }
-
-    const menu = result.data.getMenuAtLocation;
-
-    const parsedResult = {
-      title: menu.title,
-      menuItems: menu.menuItems.map((item: any) => parseMenuItem(item)),
-    };
-
-    return parsedResult;
+  protected parseResult(result: any): any {
+    return result;
   }
 }
