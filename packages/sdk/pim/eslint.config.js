6658881751beb85c46875a9cc3f443a02190b8c6
arvidsson-geins <arvidsson@geins.io>
2024-11-11 16:15:29 +0100

    chore: cleaning next branch

--- packages/sdk/pim/eslint.config.js
+++
@@ -1,3 +0,0 @@
-import { geinsSharedConfig } from '@geins/eslint-config/eslint-config.js';
-
-export default [...geinsSharedConfig];
--- packages/sdk/pim/src/graphql/queries.ts
+++
@@ -1,7 +0,0 @@
-import brands from './brand/brands.gql';
-
-const queries = {
-  brands,
-};
-
-export { queries };
