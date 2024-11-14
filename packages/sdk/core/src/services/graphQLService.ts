d7416f73e7c02468d869ed4a44993ef8c309c2dd
arvidsson-geins <arvidsson@geins.io>
2024-11-14 15:56:39 +0100

    feat: added verifyQueryOptions() to GraphQLService

--- packages/sdk/core/src/services/graphQLService.ts
+++ packages/sdk/core/src/services/graphQLService.ts
@@ -6,6 +6,31 @@ export class GraphQLService extends BaseApiService {
   constructor(client: any, geinsSettings: GeinsSettings) {
     super(client, geinsSettings);
   }
+  protected verifyQueryOptions(options: GraphQLQueryOptions): GraphQLQueryOptions {
+    if (!options.query) {
+      throw new Error('Query is required');
+    }
+    if (!(options.query?.definitions?.[0]?.variableDefinitions?.length > 0)) {
+      console.log('NO VARS');
+      return options;
+    }
+
+    const queryOptions = { ...options };
+    queryOptions.variables = this.createVariables(queryOptions.variables);
+
+    const queryVars = queryOptions.query.definitions[0].variableDefinitions.map(
+      (v: any) => v.variable.name.value,
+    );
+
+    const providedVars = Object.keys(queryOptions.variables);
+    providedVars.forEach(v => {
+      if (!queryVars.includes(v)) {
+        delete queryOptions.variables[v];
+      }
+    });
+
+    return queryOptions;
+  }
 
   /**
    * Generic method to run any GraphQL query
@@ -16,7 +41,9 @@ export class GraphQLService extends BaseApiService {
    */
   async query<T = any>(options: GraphQLQueryOptions): Promise<T | null> {
     try {
-      const result = this.cleanObject(await this.runQuery(options));
+      const queryOptions = await this.verifyQueryOptions(options);
+
+      const result = this.cleanObject(await this.runQuery(queryOptions));
       const parsedResult = this.parseResult(result);
       return parsedResult as T;
     } catch (error) {
