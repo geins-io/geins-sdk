04a01f1eed409badce288429346c60b9fc4d825d
arvidsson-geins <arvidsson@geins.io>
2024-12-13 09:33:14 +0100

    feat: added oms scaffolding and docs

--- packages/sdk/types/src/oms/cart.ts
+++ packages/sdk/types/src/oms/cart.ts
@@ -1,6 +1,33 @@
+import type { ProductType, PriceType } from '../pim/product';
+import type { CampaignType } from '../pim/campaign';
+
 export type CartType = {
   id: string;
+  items: CartItemType[];
+  promoCode?: string;
+  freeShipping: boolean;
+  fixedDiscount: number;
+  merchantData?: string;
+  appliedCampaigns: any[];
+  summary: any;
 };
+
 export type CartItemType = {
+  product: ProductType;
+  skuId: number;
   id: string;
+  totalPrice: PriceType;
+  unitPrice: PriceType;
+  quantity: number;
+  campaign: CampaignType;
+  groupKey?: string;
+  productPackage?: ProductPackageCartItemType;
+  message?: string;
+};
+
+export type ProductPackageCartItemType = {
+  packageId: number;
+  packageName: string;
+  groupId: number;
+  optionId: number;
 };
