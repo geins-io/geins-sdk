23884aa17efc7259547b76b952458ece48c5b246
arvidsson-geins <arvidsson@geins.io>
2025-01-21 13:22:23 +0100

    chore: wf

--- apps/nuxt-app/nuxt.config.ts
+++ apps/nuxt-app/nuxt.config.ts
@@ -1,8 +1,5 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  nitro: {
-    preset: 'azure-swa',
-  },
   devtools: { enabled: true },
 });
