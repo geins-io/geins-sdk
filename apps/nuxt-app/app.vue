dd380f75efa295529e0d7b0f3af863ad0935045b
arvidsson-geins <arvidsson@geins.io>
2025-01-21 16:34:02 +0100

    chore: wf

--- apps/nuxt-app/app.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1>Web App</h1>
-  </div>
-</template>
--- apps/nuxt-app/nuxt.config.ts
+++
@@ -1,5 +0,0 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-});
