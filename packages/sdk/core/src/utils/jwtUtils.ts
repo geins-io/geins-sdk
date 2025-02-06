edebda1b57713550927abed31b54b7bed50ddee6
Kristian Arvidsson <124673544+arvidsson-geins@users.noreply.github.com>
2025-02-06 15:56:36 +0100

    feat: jwt-token (#121)
    * feat: jwt token
    
    * chore: update gitignore
    
    * feat: add default values to tokens

--- packages/sdk/core/src/utils/jwtUtils.ts
+++ packages/sdk/core/src/utils/jwtUtils.ts
@@ -14,7 +14,7 @@ export function decodeJWT(token: string, secretKey?: string): { header: any; pay
   }
 
   const parts = token.split('.');
-  if (parts.length !== 3) {
+  if (parts.length !== 3 && secretKey || parts.length !== 2 && !secretKey) {
     throw new Error('Invalid JWT token format.');
   }
 
@@ -80,7 +80,7 @@ export function encodeJWT(payload: Record<string, any>, secretKey?: string): str
 
   if (!secretKey) {
     // Return unsigned token if no secretKey is provided
-    return `${encodedHeader}.${encodedPayload}.`;
+    return `${encodedHeader}.${encodedPayload}`;
   }
 
   // Create the signature
