ae932fa282b19c292035007251afa2fb85bac1b7
arvidsson-geins <arvidsson@geins.io>
2025-05-23 13:28:10 +0200

    chore: init

--- packages/sdk/pim/src/geinsPIM.ts
+++ packages/sdk/pim/src/geinsPIM.ts
@@ -1,12 +1,27 @@
 import { BasePackage, GeinsCore } from '@geins/core';
+import { BrandService } from './services';
+export type PIMSettings = {};
 export interface GeinsPIMInterface {}
 export class GeinsPIM extends BasePackage {
-  constructor(core: GeinsCore) {
+  private _brand!: BrandService;
+
+  constructor(core: GeinsCore, options?: { pimSettings?: PIMSettings }) {
     super(core);
     const { client, geinsSettings } = core;
     this._geinsSettings = geinsSettings;
     this._apiClient = () => client ?? undefined;
     this.initServices();
+  }
+
+  get brand(): BrandService {
+    if (!this._brand) {
+      this._brand = new BrandService(() => this._apiClient(), this._geinsSettings, {});
+    }
+    return this._brand;
+  }
+
+  destroy(): void {
+    this._brand.destroy();
   }
 
   private async initServices(): Promise<void> {
--- packages/sdk/pim/src/services/brandService.ts
+++ packages/sdk/pim/src/services/brandService.ts
@@ -1,4 +1,9 @@
+import type { GeinsSettings } from '@geins/core';
 import { BaseApiService } from '@geins/core';
 // import { queries } from '../graphql';
 
-export class BrandService extends BaseApiService {}
+export class BrandService extends BaseApiService {
+  constructor(apiClient: any, geinsSettings: GeinsSettings, _settings: any) {
+    super(apiClient, geinsSettings);
+  }
+}
